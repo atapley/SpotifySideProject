72f24aba1feb8be1c3ea14752c1d84b1b367463e
atapley <tapleyalx@gmail.com>
2020-09-12 17:50:08 -0400

    New data for genre by year between 2010-2019

--- spotify_pull.py
+++ spotify_pull.py
@@ -15,27 +15,26 @@ client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secr
 # Access the API with these credentials
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-genre_list = ["yacht-rock", "pop", "country", "hip-hop", "indie-pop", "jazz", "edm", "metal", "punk", "classical"]
+genre_list = ["rock", "pop", "hip-hop", "country"]
+years_list = range(2010,2020)
 
 for genre in genre_list:
     # create empty lists where the results are going to be stored
     artist_name = []
     track_name = []
-    popularity = []
     track_id = []
 
-    for i in range(0,2000,50):
-        track_results = sp.search(q='year:2019 genre:'+genre, type='track', market='US',limit=50,offset=i)
-        for i, t in enumerate(track_results['tracks']['items']):
-            artist_name.append(t['artists'][0]['name'])
-            track_name.append(t['name'])
-            track_id.append(t['id'])
-            popularity.append(t['popularity'])
+    for year in years_list:
+        for i in range(0,2000,50):
+            track_results = sp.search(q='year:'+str(year) + ' genre:'+genre, type='track', market='US',limit=50,offset=i)
+            for i, t in enumerate(track_results['tracks']['items']):
+                artist_name.append(t['artists'][0]['name'])
+                track_name.append(t['name'])
+                track_id.append(t['id'])
+
+        print(str(year) + ' completed!')
 
     df_tracks = pd.DataFrame(
-        {'artist_name': artist_name, 'track_name': track_name, 'track_id': track_id, 'popularity': popularity})
+        {'artist_name': artist_name, 'track_name': track_name, 'track_id': track_id})
     df_tracks.to_csv('./data/' + genre + '.csv')
     print(genre + ' completed!')
-    plt.hist(x=popularity, bins=50);
-    plt.savefig('./hist/' + genre + '.png')
-    plt.clf()
