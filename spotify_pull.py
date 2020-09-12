a3a65f1a865f2d95b8a0aed60351bdd6222f887e
Brendyn Delage <brendynd@live.com>
2020-09-12 12:45:40 -0400

    aaaaaa`

--- spotify_pull.py
+++ spotify_pull.py
@@ -7,6 +7,5 @@ secret = "a8eebb34f19e4494a93cbd05b772e7cc"
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-
-artist = sp.artist('spotify:artist:4tZwfgrHOc3mvqYlEYSvVi')
-print(artist)
+result = sp.search(q='country')
+print(result)
